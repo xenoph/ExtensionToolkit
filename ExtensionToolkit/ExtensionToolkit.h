7edbd25b90c2ee5540559d19b2e62f7d98f529af
Bjørn Vidar Dahle <bvidar@dahle.me>
2018-03-12 11:46:42 +0100

    Initial Commit

---
+++ ExtensionToolkit/ExtensionToolkit.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionToolkit.h
+//  ExtensionToolkit
+//
+//  Created by Bjørn Vidar Dahle on 12/03/2018.
+//  Copyright © 2018 Bjørn Vidar Dahle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtensionToolkit.
+FOUNDATION_EXPORT double ExtensionToolkitVersionNumber;
+
+//! Project version string for ExtensionToolkit.
+FOUNDATION_EXPORT const unsigned char ExtensionToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionToolkit/PublicHeader.h>
+
+
